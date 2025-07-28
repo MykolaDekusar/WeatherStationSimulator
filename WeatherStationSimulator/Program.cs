6009455f6e6e4ef4cd2c8e6874ba2e00ee701197
MykolaDekusar <ndekusar97@gmail.com>
2025-07-29 00:21:46 +0200

    Added population of arrays based on user choices of how many days to take into account, random weather condition based on a predetermined conditions array, min temperature and max temperature

--- WeatherStationSimulator/Program.cs
+++ WeatherStationSimulator/Program.cs
@@ -2,6 +2,8 @@
 {
     internal class Program
     {
+        static int minTemp;
+        static int maxTemp;
         static void Main(string[] args)
         {
             Console.WriteLine("Inserisci il numero di giorni da simulare");
@@ -9,7 +11,20 @@
             int[] temperature = new int[days];
             string[] conditions = { "Solleggiato", "Piovoso", "Nuvoloso", "Nevoso" };
             string[] weatherConditions = new string[days];
+            Console.WriteLine("Inserisci la temperatura minima possibile");
+            minTemp = int.Parse(Console.ReadLine());
+            Console.WriteLine("Inserisci la temperatura massima possibile");
+            maxTemp = int.Parse(Console.ReadLine());
 
+            Random random = new Random();
+            for (int i = 0; i < days; i++)
+            {
+                temperature[i] = random.Next(minTemp, maxTemp+1);
+                weatherConditions[i] = conditions[random.Next(conditions.Length)];
+                Console.WriteLine(temperature[i]);
+            }
+
+            
 
             Console.ReadKey();
 
