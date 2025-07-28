81e91bdee021eea8d1f0a17d3b289f983d6baadf
MykolaDekusar <ndekusar97@gmail.com>
2025-07-29 00:12:47 +0200

    Initialization of variables and string for data collection

--- WeatherStationSimulator/Program.cs
+++ WeatherStationSimulator/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Inserisci il numero di giorni da simulare");
+            int days = int.Parse(Console.ReadLine());
+            int[] temperature = new int[days];
+            string[] conditions = { "Solleggiato", "Piovoso", "Nuvoloso", "Nevoso" };
+            string[] weatherConditions = new string[days];
+
+
+            Console.ReadKey();
+
         }
     }
 }
